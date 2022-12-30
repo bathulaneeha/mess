0709b8ad302500bf0c6678868344352891a95ed0
neehabathula <neehabathula@gmail.com>
2022-12-30 17:56:50 +0530

    mess

--- src/components/admin.js
+++ src/components/admin.js
@@ -5,57 +5,93 @@ import {BsReception3, BsFillFilePersonFill,BsMailbox,BsFillTelephoneFill, BsBook
 import { AiTwotoneMail } from "react-icons/ai";
 function Admin() {
  return (
-    <div className='container-fluid mt-5 text-light'  >
-      <div className="bg-light w-50 rounded mx-auto" style={{backgroundImage:`Url(https://media.istockphoto.com/id/1300397135/vector/violet-purple-and-navy-blue-defocused-blurred-motion-gradient-abstract-background.jpg?s=612x612&w=0&k=20&c=YH_QbC3h3uaxsr9X55MG4oeeySjmSXHL8yKTYVYfsSU=)`,backgroundRepeat:'NoRepeat', backgroundSize:'cover',marginTop:'6rem'}}>
-      <div className="display-4 text-center  mt-5 mb-2">Admin SignUp</div>
-      <div className="row">
-        <div className="col-12 col-sm-8 col-md-6  mx-auto">
-      <Form>
-        <Form.Group className="mb-3">
-          <Form.Label> <BsFillFilePersonFill className='me-2'/>Name</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Ravinder Kumar"
-            
-          />
-          
-        </Form.Group>
-        <Form.Group className="mb-3">
-          <Form.Label> <AiTwotoneMail  className='me-2'/>Email ID</Form.Label>
-          <Form.Control
-            type="mail"
-            placeholder="Enter Email ID"
-            
-          />
-          
-        </Form.Group>
-        <Form.Group className="mb-3">
-          <Form.Label> <BsFillTelephoneFill className='me-2'/>Phone Number</Form.Label>
-          <Form.Control
-            type="number"
-            placeholder="Enter Phone Number"
-            
-          />
-          
-        </Form.Group>
-        <Form.Group className="mb-3">
-          <Form.Label> <BsBook className='me-2'/>Class & Section</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Enter class and Section"
-            
-          />
-          
-        </Form.Group>
-      </Form>
-      <Button  variant="light" type="submit" className="mb-3">
-          Sign Up
-        </Button>
-      </div>
-      </div>
-      </div>
-      
-    </div>
+    <div>
+      <h1 className="text-center text-primary mb-4">MESS IS MESS?</h1>
+      <div className="col-sm-8 container-fluid">
+              <nav className="nav-bar navbar-expand-lg navbar-light bg-light px-md-5 py-md-3">
+                <a className="navbar-brand text-center" href="/"> <strong>
+                    College Canteen
+                  </strong> </a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="true" aria-label="Toggle navigation">
+                  <span className="navbar-toggler-icon" />
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                  <ul className="navbar-nav">
+                    <li className="nav-item">
+                      <a href="http://127.0.0.1:5501/admin.html" className="nav-link navtext me-5">Admin</a>
+                    </li>
+                    <li className="nav-item">
+                      <a href="#" className="nav-link navtext"> Student </a>
+                    </li>
+                  </ul>
+                </div>
+              </nav> 
+              <h1 className="text-center text-secondary mt-3 mb-3"> MENU</h1>
+              <table className="table table-dark table-hover">
+                <thead>
+                  <tr>
+                    <th scope="col">
+                      SNo
+                    </th>
+                    <th scope="col">
+                      Name
+                    </th>
+                    <th scope="col">
+                      Price
+                    </th>
+                    <th scope="col">
+                      Remaining Plates
+                    </th>
+                    <th scope="col">
+                      Buy?
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>1</td>
+                    <td>Veg Biryani</td>
+                    <td>40</td>
+                    <td id="biryani">100</td>
+                    <td><button className="btn btn-info" id="btn1">Select</button></td>
+                  </tr>
+                  <tr>
+                    <td>2</td>
+                    <td>Paratha</td>
+                    <td>30</td>
+                    <td id="paratha">80</td>
+                    <td><button className="btn btn-info" id="btn2">Select</button></td>
+                  </tr>
+                  <tr>
+                    <td>3</td>
+                    <td>Chapathi</td>
+                    <td>30</td>
+                    <td id="chapathi">10</td>
+                    <td><button className="btn btn-info" id="btn3">Select</button></td>
+                  </tr>
+                  <tr>
+                    <td>4</td>
+                    <td>Double ka meeta</td>
+                    <td>15</td>
+                    <td id="dkm">100</td>
+                    <td><button className="btn btn-info" id="btn4">Select</button></td>
+                  </tr>
+                  <tr>
+                    <td>5</td>
+                    <td>Puri</td>
+                    <td>30</td>
+                    <td id="puri">50</td>
+                    <td><button className="btn btn-info" id="btn5">Select</button></td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <div className="text-center">
+                <button className="btn btn-success mx-md-5" id="click">
+                  Place Order
+                </button>
+              </div>
+          </div>
   );
 }
 
